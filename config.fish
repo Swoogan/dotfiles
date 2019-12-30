73b23c8887807e5cbd7d5e8bcb9e2c18afef21c6
Colin Svingen <swoogan@gmail.com>
2019-12-29 20:49:47 -0500

    Adds fish config file

---
+++ config.fish
@@ -0,0 +1,10 @@
+set -gx PATH /snap/bin $PATH
+
+if test -z (pgrep ssh-agent)
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+
+source ~/.local/config.fish
