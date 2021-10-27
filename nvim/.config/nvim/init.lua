c976855a3f5d0c38a1d03479d2fc3591055d8124
Colin Svingen <swoogan@gmail.com>
2021-10-27 08:29:07 -0400

    Adds C# snippets to nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -334,14 +334,47 @@ _G.s_tab_complete = function()
     return ""
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
-
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
-vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local s = luasnip.snippet
+local sn = luasnip.snippet_node
+local t = luasnip.text_node
+local i = luasnip.insert_node
+local f = luasnip.function_node
+local c = luasnip.choice_node
+local d = luasnip.dynamic_node
+
+luasnip.snippets = {
+    cs = {
+        s("sum", { 
+            t({"/// <summary>", "/// "}),
+            i(0),
+            t({"", "/// </summary>"}),
+        }),
+        s("pra", {
+            t("<parameter name=\""),
+            i(1, "name"),
+            t("\">"),
+            i(0),
+            t("</parameter>"),
+        }),
+        s({trig = "try", name = "Try log", dscr = "Try catch, log the exception"}, { 
+            t({"try", "{", "\t"}),
+            i(0),
+            t({"","}", "catch ("}),
+            i(1, "Exception"),
+            t(" e)"),
+            t({"", "{", "\t_logger.LogError(e, \""}),
+            i(2),
+            t({"\");", "}"}),
+        }),
+    }
+}
 
 -- *** End luasnips
 
