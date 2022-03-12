d3169594128139686192bfc2ead593a170791cc6
Colin Svingen <swoogan@gmail.com>
2022-03-12 11:44:59 -0500

    Changes nvim-compe to nvim-cmp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -29,7 +29,12 @@ require('packer').startup(function()
   use 'theHamsta/nvim-dap-virtual-text'
   use 'mfussenegger/nvim-dap'
 
-  use 'hrsh7th/nvim-compe'  -- Autocomplete
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'  -- Autocomplete
+
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }  -- Fuzzy finder
   use 'prettier/vim-prettier'  -- Run prettier formatting for javascript/typescript
   use { 
@@ -53,6 +58,7 @@ require('packer').startup(function()
   -- use 'tpope/vim-unimpaired' 
   use 'tpope/vim-dispatch' -- Async task runner
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'saadparwaiz1/cmp_luasnip' -- luasnip to nvim-cmp integration
   use 'rafamadriz/friendly-snippets'  -- Premade snippets
   use 'AndrewRadev/tagalong.vim' -- HTML tag manipulation (eg: edit in tag, <|div></div> ciwspan -> <span></span>
 end)
@@ -143,40 +149,69 @@ nvim_lsp['omnisharp'].setup {
 }
 
 -- Setup auto compeletion
-vim.o.completeopt = "menuone,noselect"
+vim.o.completeopt = "menu,menuone,noselect"
 
-require('compe').setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  resolve_timeout = 800;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = {
-    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-    max_width = 120,
-    min_width = 60,
-    max_height = math.floor(vim.o.lines * 0.3),
-    min_height = 1,
-  };
+-- Setup nvim-cmp.
+local cmp = require('cmp')
 
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    spell = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    luasnip = true;
-    treesitter = false;
-  };
+cmp.setup({
+    snippet = {
+      expand = function(args)
+	require('luasnip').lsp_expand(args.body) 
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+	i = cmp.mapping.abort(),
+	c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+    }, {
+      { name = 'buffer' },
+    })
+})
+
+-- Set configuration for specific filetype.
+--    cmp.setup.filetype('gitcommit', {
+    -- sources = cmp.config.sources({
+    -- { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it. 
+    -- }, {
+    --     { name = 'buffer' },
+    --     })
+--    })
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+sources = {
+  { name = 'buffer' }
+}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+sources = cmp.config.sources({
+  { name = 'path' }
+}, {
+  { name = 'cmdline' }
+})
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['omnisharp'].setup {
+    capabilities = capabilities
+}
+require('lspconfig')['tsserver'].setup {
+    capabilities = capabilities
 }
 
 --Set statusbar
