2eebbcb07b391e17f43c43494767a639e36a3e71
Colin Svingen <swoogan@gmail.com>
2023-07-26 15:01:29 -0400

    [nvim] disables pylint, enables ruff

--- nvim/.config/nvim/lua/config/lang.lua
+++ nvim/.config/nvim/lua/config/lang.lua
@@ -201,7 +201,7 @@ M.setup = function()
     sources = {
       null_ls.builtins.formatting.black,
       null_ls.builtins.diagnostics.flake8,
-      null_ls.builtins.diagnostics.pylint,
+      null_ls.builtins.diagnostics.ruff,
     },
   })
 
