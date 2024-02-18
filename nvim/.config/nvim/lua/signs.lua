4f75d17dc864083286b1a1353965edf991e75a72
Colin Svingen <swoogan@gmail.com>
2024-02-18 13:38:28 -0500

    [nvim] Fixes sign_marks only allowing one per session, instead of per buffer

--- nvim/.config/nvim/lua/signs.lua
+++ nvim/.config/nvim/lua/signs.lua
@@ -1,43 +1,45 @@
 local M = {
   group = 'marks',
-  names = { m = "MarkAnchor", s = "MarkStart", e = "MarkEnd" }
+  names = { m = "MarkAnchor", s = "MarkStart", e = "MarkEnd" },
+  data = {
+    anchor = { mark = 'm', sign = 'MarkAnchor', text = '⚓' },
+    start = { mark = 's', sign = 'MarkStart', text = '🔻' },
+    ['end'] = { mark = 'e', sign = 'MarkEnd', text = '🔺' },
+  }
 }
 
-M.set_mark = function(id, name)
+M.set_mark = function(name)
   local bufnr = vim.api.nvim_get_current_buf()
   local lnum, cnum = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.fn.sign_unplace(M.group, { id = id })
-  vim.fn.sign_place(id, M.group, M.names[name], bufnr, { lnum = lnum })
-  vim.api.nvim_buf_set_mark(bufnr, name, lnum, cnum, {})
+  local sign = M.data[name]
+  vim.fn.sign_unplace(name, { id = bufnr })
+  vim.fn.sign_place(bufnr, name, sign.sign, bufnr, { lnum = lnum })
+  vim.api.nvim_buf_set_mark(bufnr, sign.mark, lnum, cnum, {})
 end
 
 M.get_all = function()
-  local curr_bufnr = vim.api.nvim_get_current_buf()
-  local placed_signs = vim.fn.sign_getplaced(curr_bufnr, { group = M.group })
+  local bufnr = vim.api.nvim_get_current_buf()
 
   local result = {}
-  for key, placed_sign in pairs(placed_signs) do
-    local new_placed_sign = { bufnr = placed_sign.bufnr, signs = {} }
-    local signs = placed_sign.signs
-    local mark_name = ""
-    for _, sign in pairs(signs) do
-      for mark, name in pairs(M.names) do
-        if sign.name == name then
-          mark_name = mark
-        end
+  for name, sign in pairs(M.data) do
+    local placed_signs = vim.fn.sign_getplaced(bufnr, { group = name })
+
+    for _, placed_sign in pairs(placed_signs) do
+      local new_placed_sign = { bufnr = placed_sign.bufnr, signs = {} }
+      for _, existing in pairs(placed_sign.signs) do
+        local _, cnum = unpack(vim.api.nvim_buf_get_mark(bufnr, sign.mark))
+        local new_sign = {
+          id = existing.id,
+          name = sign.sign,
+          group = existing.group,
+          lnum = existing.lnum,
+          cnum = cnum,
+          priority = existing.priority
+        }
+        table.insert(new_placed_sign.signs, new_sign)
       end
-      local _, cnum = unpack(vim.api.nvim_buf_get_mark(curr_bufnr, mark_name))
-      local new_sign = {
-        id = sign.id,
-        name = sign.name,
-        group = sign.group,
-        lnum = sign.lnum,
-        cnum = cnum,
-        priority = sign.priority
-      }
-      table.insert(new_placed_sign.signs, new_sign)
+      table.insert(result, new_placed_sign)
     end
-    table.insert(result, new_placed_sign)
   end
   return result
 end
@@ -59,29 +61,29 @@ M.set_all = function(placed_signs)
 end
 
 M.set_anchor = function()
-  M.set_mark(1, "m")
+  M.set_mark("anchor")
 end
 
 -- aka top
 M.set_start = function()
-  M.set_mark(5, "s")
+  M.set_mark("start")
 end
 
 -- aka bottom
 M.set_end = function()
-  M.set_mark(10, "e")
+  M.set_mark("end")
 end
 
 M.setup = function()
-  vim.fn.sign_define('MarkAnchor', { text = '⚓' })
-  vim.fn.sign_define('MarkStart', { text = '🔻' })
-  vim.fn.sign_define('MarkEnd', { text = '🔺' })
-
   -- todo: this doesn't belong here
   vim.fn.sign_define('ImportantLine', { text = '🌟' })
 
-  -- signs = { 'm', 'a', 's', 't', 'n', 'e' }
-  -- signs = { 'a', 's', 't', 'n', 'e' }
+  for _, sign in pairs(M.data) do
+    vim.fn.sign_define(sign.sign, { text = sign.text })
+  end
+
+
+  -- signs = { 'a', 't', 'n', 'o' }
   -- for _, sign in pairs(signs) do
   --   vim.fn.sign_define('Mark_' .. sign, { text = sign })
   -- end
