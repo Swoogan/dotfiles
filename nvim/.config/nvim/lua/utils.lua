1dea4f5d122fa6a4d0b1ef7e8443561d2a4be249
Colin Svingen <swoogan@gmail.com>
2024-10-29 13:33:46 -0400

    [nvim] Moves the cpp build runner to generic utils function

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -1,4 +1,8 @@
 local M = {
+  OutputTargets = {
+    Stdout = 1,
+    Stderr = 2
+  }
 }
 
 local find_next_line = function(bufnr, direction, start, match)
@@ -97,5 +101,66 @@ M.create_qf_entries = function(parsed_results)
 
   return entries
 end
+
+--- Run an application in the background and write its output to a new buffer
+---@param command string[] The command and its arguments
+---@param completion_msg string The message to display upon success
+---@param output_source integer Output for the command 1: stdout, 2: stderr
+M.run_buffered = function(command, completion_msg, output_source)
+  output_source = output_source or M.OutputTargets.Stdout
+
+  -- first save your current window's id, so that we can restore the size later
+  local initial_win_id = vim.api.nvim_get_current_win()
+
+  -- Create a new horizontal split new window at the bottom of the current window
+  vim.cmd('botright split')
+
+  -- Get the new window's id
+  local new_win_id = vim.api.nvim_get_current_win()
+
+  -- Get the maximum window height, we will use this to calculate 40%
+  local total_height = vim.api.nvim_win_get_height(initial_win_id)
+
+  -- Set the height of our new window to be approximately 40% of the total height.
+  -- cgs: there's no way this is 40% of the window...
+  vim.api.nvim_win_set_height(new_win_id, math.floor(total_height * 0.4))
+
+  -- Create a new buffer
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  -- Set that buffer in the new window we have created
+  vim.api.nvim_win_set_buf(new_win_id, buf)
+
+  local function write_output(_, output)
+    if output then
+      vim.schedule(function()
+        local new_text = vim.split(output, '\r?\n', { trimempty = true })
+        vim.api.nvim_buf_set_lines(buf, -1, -1, false, new_text)
+        local last_line = vim.api.nvim_buf_line_count(buf)
+        vim.api.nvim_win_set_cursor(new_win_id, { last_line, 0 })
+      end)
+    end
+  end
+
+  local options
+  if output_source == M.OutputTargets.Stderr then
+    options = { stderr = write_output }
+  else
+    options = { stdout = write_output }
+  end
+
+  local job = vim.system(command, options, function(_)
+    vim.schedule(function()
+      vim.api.nvim_echo({ { completion_msg, 'InfoMsg' } }, true, {})
+      -- Set focus back to the initial window
+      vim.api.nvim_set_current_win(initial_win_id)
+    end)
+  end)
+  if job.pid <= 0 then
+    vim.api.nvim_echo({ { 'Failed to start the job!', 'ErrorMsg' } }, true, {})
+  end
+end
+
+
 
 return M
