4cc3e3cedc45c8c08d65c90bd87c51c5767662e9
Colin Svingen <swoogan@gmail.com>
2025-02-14 10:22:32 -0500

    [nvim] Fixes wrong indent when using print_debug

--- nvim/.config/nvim/lua/print_debug.lua
+++ nvim/.config/nvim/lua/print_debug.lua
@@ -1,29 +1,29 @@
 local M = {}
 
-local function set_line_number(lnum, cnum)
-  vim.api.nvim_buf_set_text(0, lnum - 1, cnum + 1, lnum - 1, cnum + 1, { '(' .. tostring(lnum) .. ')' })
+local function generate_logging(format)
+  local lnum, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.cmd.normal('yiw')
+  local variable = vim.fn.getreg('"')
+  local new_line = string.format(format, variable, lnum, variable)
+  local current_indent = vim.fn.indent(lnum)
+  local indent_str = string.rep(' ', current_indent)
+  vim.api.nvim_put({ indent_str .. new_line }, 'l', true, true)
+  vim.cmd.normal('k$')
 end
 
 M.lua_print = function()
-  vim.cmd.normal('yiwovim.print("" ')
-  local lnum, cnum = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd.normal('a:", ")')
-  set_line_number(lnum, cnum)
-  vim.cmd.normal('^')
+  -- vim.print("lnum (23):", lnum)
+  generate_logging('print("%s (%d):" .. vim.inspect(%s))')
 end
 
 M.python_print = function()
-  vim.cmd.normal('yiwoprint(f"" ')
-  local lnum, cnum = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd.normal('a: {"}")')
-  set_line_number(lnum, cnum)
+  -- print(f"issues (302): {issues}")
+  generate_logging('print(f"%s (%d): {%s}")')
 end
 
 M.print_rust = function()
-  vim.cmd.normal('yiwoprintln!("" ')
-  local lnum, cnum = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd.normal('a: {}", ");')
-  set_line_number(lnum, cnum)
+  -- println!("env (66): {}", env);
+  generate_logging('println!("%s (%d): {}", %s);')
 end
 
 return M
