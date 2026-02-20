bcc7c508fe2a0143562d4b96ba72eb1631709155
Colin Svingen <swoogan@gmail.com>
2026-02-20 18:57:29 -0500

    Adds cross buffer text completions

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -195,7 +195,18 @@ M.spec = {
             { name = 'luasnip' },
             { name = 'path' },
           }, {
-            { name = 'buffer' },
+            {
+              name = 'buffer',
+              option = {
+                get_bufnrs = function()
+                  local bufs = {}
+                  for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    bufs[vim.api.nvim_win_get_buf(win)] = true
+                  end
+                  return vim.tbl_keys(bufs)
+                end
+              }
+            },
           }
         ),
         -- defaults
@@ -233,7 +244,18 @@ M.spec = {
             { name = 'path' },
           },
           {
-            { name = 'buffer' },
+            {
+              name = 'buffer',
+              option = {
+                get_bufnrs = function()
+                  local bufs = {}
+                  for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    bufs[vim.api.nvim_win_get_buf(win)] = true
+                  end
+                  return vim.tbl_keys(bufs)
+                end
+              }
+            },
           }
         )
       })
@@ -253,7 +275,18 @@ M.spec = {
             { name = 'path' },
           },
           {
-            { name = 'buffer' },
+            {
+              name = 'buffer',
+              option = {
+                get_bufnrs = function()
+                  local bufs = {}
+                  for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    bufs[vim.api.nvim_win_get_buf(win)] = true
+                  end
+                  return vim.tbl_keys(bufs)
+                end
+              }
+            },
           }
         )
       })
